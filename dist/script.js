96bc635c6faa594c587338912d999f80bf1bff92
Marina <do-you-speak@yandex.ru>
2016-03-03 22:05:33 +0300

    Add rotation

--- dist/script.js
+++ dist/script.js
@@ -8,7 +8,7 @@ var canvas = document.getElementById('gameCanvas');
 var ctx = canvas.getContext("2d");
 
 var Ship = function () {
-  function Ship(radius, color, posX, posY, startAngle, speed) {
+  function Ship(radius, color, posX, posY, startAngle, endAngle, speed) {
     _classCallCheck(this, Ship);
 
     this.radius = radius;
@@ -16,6 +16,7 @@ var Ship = function () {
     this.posX = posX;
     this.posY = posY;
     this.startAngle = startAngle;
+    this.endAngle = endAngle;
     this.speed = speed;
   }
 
@@ -24,7 +25,7 @@ var Ship = function () {
     value: function draw() {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       ctx.beginPath();
-      ctx.arc(this.posX, this.posY, this.radius, this.startAngle, Math.PI * 1.5);
+      ctx.arc(this.posX, this.posY, this.radius, Math.PI * this.startAngle, Math.PI * this.endAngle);
       ctx.fillStyle = this.color;
       ctx.fill();
       ctx.closePath();
@@ -49,6 +50,18 @@ var Ship = function () {
     value: function moveRight() {
       this.posX += this.speed;
     }
+  }, {
+    key: "rotateLeft",
+    value: function rotateLeft() {
+      this.startAngle -= 0.1;
+      this.endAngle -= 0.1;
+    }
+  }, {
+    key: "rotateRight",
+    value: function rotateRight() {
+      this.startAngle += 0.1;
+      this.endAngle += 0.1;
+    }
   }]);
 
   return Ship;
@@ -58,6 +71,7 @@ var Ship = function () {
 
 
 document.onkeydown = function (e) {
+  console.log(e.keyCode);
   if (e.keyCode == 38) {
     myShip.moveUp();
   } else if (e.keyCode == 40) {
@@ -66,9 +80,13 @@ document.onkeydown = function (e) {
     myShip.moveLeft();
   } else if (e.keyCode == 39) {
     myShip.moveRight();
+  } else if (e.keyCode == 65) {
+    myShip.rotateLeft();
+  } else if (e.keyCode == 68) {
+    myShip.rotateRight();
   }
   myShip.draw();
 };
 
-var myShip = new Ship(30, "white", canvas.width / 2, canvas.height / 2, 0, 5);
+var myShip = new Ship(30, "white", canvas.width / 2, canvas.height / 2, 1, 2.5, 5);
 myShip.draw();
--- js/script.js
+++ js/script.js
@@ -4,19 +4,20 @@ const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext("2d");
 
 class Ship {
-  constructor(radius, color, posX, posY, startAngle, speed) {
+  constructor(radius, color, posX, posY, startAngle, endAngle, speed) {
     this.radius = radius;
     this.color = color;
     this.posX = posX;
     this.posY = posY;
     this.startAngle = startAngle;
+    this.endAngle = endAngle;
     this.speed = speed;
   }
 
   draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
-    ctx.arc(this.posX, this.posY, this.radius, this.startAngle, Math.PI*1.5);
+    ctx.arc(this.posX, this.posY, this.radius, Math.PI*this.startAngle, Math.PI*this.endAngle);
     ctx.fillStyle = this.color;
     ctx.fill();
     ctx.closePath();
@@ -37,10 +38,21 @@ class Ship {
   moveRight() {
     this.posX += this.speed;
   }
+
+  rotateLeft() {
+    this.startAngle -= 0.1;
+    this.endAngle -= 0.1;
+  }
+
+  rotateRight() {
+    this.startAngle += 0.1;
+    this.endAngle += 0.1;
+  }
 }
 
 // Listen to key press
 document.onkeydown = function(e){
+  console.log(e.keyCode);
   if(e.keyCode == 38){
     myShip.moveUp();
   } else if(e.keyCode == 40) {
@@ -49,9 +61,13 @@ document.onkeydown = function(e){
     myShip.moveLeft();
   } else if(e.keyCode == 39) {
     myShip.moveRight();
+  } else if(e.keyCode == 65) {
+    myShip.rotateLeft();
+  } else if(e.keyCode == 68) {
+    myShip.rotateRight();
   }
   myShip.draw();
 };
 
-var myShip = new Ship(30, "white", canvas.width/2, canvas.height/2, 0, 5);
+var myShip = new Ship(30, "white", canvas.width/2, canvas.height/2, 1, 2.5, 5);
 myShip.draw();
